bad21c7917d0dc0d6d6382c3673100ccad6e9531
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-09-01 23:47:54 +0400

    testing

--- index.js
+++ index.js
@@ -3,20 +3,12 @@ const content = document.querySelector('.content');
 const hidden = document.querySelector('.hidden');
 const img = document.querySelector('img');
 
-
-
-
 //play preloader for 3seconds
 window.addEventListener('load', function () {
   setTimeout(() => {
     preloader.classList.add('hidden');
     content.classList.remove('hidden');
-    content.classList.add('test');
   }, 3000);
 
-  //then load content
-  setTimeout(() => {
-    content.classList.remove('test');
-  }, 8000);
 
 })
