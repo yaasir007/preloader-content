b1d3756096936098230162a7595279b09d531e30
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-09-02 23:17:42 +0400

    changes unfinished

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const preloader = document.querySelector('.lds-ellipsis');
 const content = document.querySelector('.content');
-const hidden = document.querySelector('.hidden');
-const img = document.querySelector('img');
+// const hidden = document.querySelector('.hidden');
+// const img = document.querySelector('img');
 
-//play preloader for 3seconds
-window.addEventListener('load', function () {
-  setTimeout(() => {
-    preloader.classList.add('hidden');
-    content.classList.remove('hidden');
-  }, 3000);
+// //play preloader for 3seconds
+// window.addEventListener('load', function () {
+//   setTimeout(() => {
+//     preloader.classList.add('hidden');
+//     content.classList.remove('hidden');
+//   }, 3000);
 
 
-})
+// })
+
+const fadeOutEffect = setInterval(() => {
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+}, 300);
+
+window.addEventListener('load', fadeOutEffect);
+content.style.display ='block';
