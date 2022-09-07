8bae702a32be4feec6336c21740fb5c4adde889c
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-09-07 22:53:46 +0400

    reset css

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const preloader = document.querySelector('.lds-ellipsis');
 const content = document.querySelector('.content');
-// const hidden = document.querySelector('.hidden');
-// const img = document.querySelector('img');
-
-// //play preloader for 3seconds
-// window.addEventListener('load', function () {
-//   setTimeout(() => {
-//     preloader.classList.add('hidden');
-//     content.classList.remove('hidden');
-//   }, 3000);
-
-
-// })
 
 const fadeOutEffect = setInterval(() => {
-  
-}, 300);
+  preloader.classList.add('hidden')
+  content.classList.remove('hidden')
+}, 3000);
 
 window.addEventListener('load', fadeOutEffect);
-content.style.display ='block';
