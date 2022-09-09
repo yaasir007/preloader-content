5220220ccb52c1dd72b9051edd4d1dcf7184a251
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-09-10 01:35:51 +0400

    added aniJS

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const preloader = document.querySelector('.lds-ellipsis');
 const content = document.querySelector('.content');
 
 const fadeOutEffect = setInterval(() => {
-  preloader.classList.add('hidden')
+  preloader.classList.add('hidden');
+  content.classList.remove('hidden');
 }, 3000);
 
 window.addEventListener('load', fadeOutEffect);
